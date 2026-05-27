1d587b0fa3241ff056033b08e17c15577e353dcc
Shirinemerwali <shirin.merwali@outlook.com>
2026-05-27 19:19:14 +0200

    Fix backend port for Render

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ dotenv.config();
 const app = express();
 
 
+// PORT
+
+const PORT = process.env.PORT || 3001;
+
+
 // MIDDLEWARE
 
 app.use(cors());
@@ -42,10 +47,10 @@ mongoose.connect(process.env.MONGO_URI)
 
   console.log("MongoDB connected");
 
-  app.listen(process.env.PORT, () => {
+  app.listen(PORT, () => {
 
     console.log(
-      `Server running on port ${process.env.PORT}`
+      `Server running on port ${PORT}`
     );
 
   });
